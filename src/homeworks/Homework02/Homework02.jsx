4f4b706714e664b1eceaaa039a70bc971b867c98
iwan-semjonow-dev <iwan.semjonow.dev@gmail.com>
2026-09-09 19:58:36 +0200

    Add homework 02

---
+++ src/homeworks/Homework02/Homework02.jsx
@@ -0,0 +1,13 @@
+import Button from "../../components/Button/Button";
+import "./styles.css";
+
+function Homework02() {
+  return (
+    <div className="homework02-wrapper">
+      <Button name="Save changes" type="button" />
+      <Button name="Submit homework" type="submit" />
+    </div>
+  );
+}
+
+export default Homework02;
