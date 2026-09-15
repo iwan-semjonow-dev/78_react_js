825b828c7472262d90268602c6ea25fce81d417e
iwan-semjonow-dev <iwan.semjonow.dev@gmail.com>
2026-09-15 07:55:48 +0200

    Add lesson 04

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import "./App.css";
 // import Lesson01 from "./lessons/Lesson01/Lesson01";
 // import Lesson02 from "./lessons/Lesson02/Lesson02";
 // import Lesson03 from "./lessons/Lesson03/Lesson03";
-
+import Lesson04 from "./lessons/Lesson04/Lesson04";
 
 // Homeworks imports
-import Homework02 from "./homeworks/Homework02/Homework02";
+// import Homework02 from "./homeworks/Homework02/Homework02";
 
 
 function App() {
@@ -18,9 +18,11 @@ function App() {
       {/* Lesson 02. Props */}
       {/* <Lesson02 /> */}
       {/* Homework 02. Props */}
-      <Homework02/>
+      {/* <Homework02/> */}
       {/* Lesson 03. Map components */}
       {/* <Lesson03/> */}
+      {/* Lesson 04. State */}
+      <Lesson04/>
     </div>
   );
 }
